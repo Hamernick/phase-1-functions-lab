507b6fce66cda0a75b38f6ccf8e93cda11c95da8
Caleb Hamernick <caleb.hamernick@gmail.com>
2022-02-24 07:25:03 -0500

    commit tree 5ac4ad1d491f8c93dc35a3eb06f09c8560be03b3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks !== 42) {
+        let userDistance = Math.abs(42 - blocks)
+        return userDistance
+    }
+}
+
+function distanceFromHqInFeet(ham) {
+    let something = distanceFromHqInBlocks(ham);
+    return something * 264
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+
+}
+
